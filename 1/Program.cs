82b7f1c6ed84cc32e7ca0337f2efe360afe0b411
kdb11 <112870790+kdb11@users.noreply.github.com>
2025-02-10 11:21:22 +0100

    Added previous local leetcode answers

---
+++ 1/Program.cs
@@ -0,0 +1,21 @@
+﻿using Solution;
+
+int[] prices = [7,1,5,3,6,4];
+
+solution2.MaxProfit(prices);
+
+int[] nums = [0,0,1,1,1,2,2,3,3,4];
+
+solution1.RemoveDuplicates(nums);
+
+/* int[] n = [3,2,2,3];
+int v = 3; */
+
+int[] n = [0,1,2,2,3,0,4,2];
+int v = 2;
+
+Solution3.RemoveElement( n, v);
+
+string[] strs = ["flower","flow","flight"];
+
+Solution4.LongestCommonPrefix(strs);
---
+++ 1/Solution/4.cs
@@ -0,0 +1,23 @@
+namespace Solution
+{
+    class Solution4
+    {
+        public static string LongestCommonPrefix(string[] strs) {
+            char commonPrefix = 'a';
+            for (int i = 0; i < strs.Length; i++)
+            {
+                /* Console.WriteLine(strs[i].CompareTo(strs[i+1])); */
+                for (int j = 0; j < strs[i].Length; j++)
+                {
+                    Console.WriteLine(strs[i][j]);
+
+                    if (strs[i - 1][j] == strs[i][j] ){
+                        Console.WriteLine("same");
+                    }
+                }
+            }
+
+            return "";
+        }
+    }
+}
